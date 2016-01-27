698b1b62b8dd8e5d7705aa99e7446825e0a8a199
fedemarske <fede.marske@gmail.com>
2016-01-27 18:10:01 -0300

    Added FOSUserBundle, KnpMenuBundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppBundle\Entity;
+
+
+class User
+{
+    public $id;
+    public $email;
+    public $password;
+    public $profId;
+    public $active;
+    public $roleId;
+    public $username;
+}
