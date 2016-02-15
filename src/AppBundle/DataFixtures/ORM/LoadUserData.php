545c8f5320b59bb04a4c76e8ee172d5a1f936f5a
Diego <diegoangel77@rocketmail.com>
2016-02-15 02:43:07 -0300

    Update bower dependencies and fixtures development

--- src/AppBundle/DataFixtures/ORM/LoadUserData.php
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -2,37 +2,127 @@
 
 namespace AppBundle\DataFixtures\ORM;
 
+use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\Company;
 use AppBundle\Entity\User;
+use AppBundle\Entity\Category;
+use AppBundle\Entity\Logo;
+use AppBundle\Entity\Location;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class LoadUserData implements FixtureInterface
+class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
 {
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     public function load(ObjectManager $manager)
     {
-    	/** System User */
-        $systemUser = new User();
+
+        $objectManager = $manager;
+
+        $testPassword = '123';
+        
+        $factory = $this->container->get('security.encoder_factory');
+        
+        /** @var $manager \FOS\UserBundle\Doctrine\UserManager */
+        $manager = $this->container->get('fos_user.user_manager');
+
+        /** System User */
+        $systemUser = $manager->createUser();
+
         $systemUser->setUsername('systemuser');
         $systemUser->setUsernameCanonical('systemuser'); 
         $systemUser->setEmail('diegoangel77@gmail.com'); 
         $systemUser->setEmailCanonical('diegoangel77@gmail.com'); 
-        $systemUser->setEnabled(1); 
-        $systemUser->setPassword('$2y$13$ljrehk4t9vkw8gsgsg4cwulJZF\/xmXA645VuovFucVGitbJpZaeNS'); 
-        $systemUser->setLastLogin(null); 
-        $systemUser->setLocked(0); 
-        $systemUser->setExpired(0); 
-        $systemUser->setExpiresAt(null); 
-        $systemUser->setConfirmationToken(null); 
-        $systemUser->setPasswordRequestedAt(null); 
-        $systemUser->setCredentialsExpired(0); 
-        $systemUser->setCredentialsExpireAt(null); 
+        $systemUser->setEnabled(true); 
+        $systemUser->setPassword('systemuser'); 
         $systemUser->setName('System User');
+        $systemUser->setRoles(array('ROLE_ADMIN'));
+        $encoder = $factory->getEncoder($systemUser);
+        $password = $encoder->encodePassword($testPassword, $systemUser->getSalt());
+        $systemUser->setPassword($password);
+
+        $manager->updateUser($systemUser);
+
+        $this->addReference('system-user', $systemUser);
 
         /** Owner User */
+        $ownerUser = $manager->createUser();
+
+        $ownerUser->setUsername('owneruser');
+        $ownerUser->setUsernameCanonical('owneruser'); 
+        $ownerUser->setEmail('diegoangel@hotmail.com'); 
+        $ownerUser->setEmailCanonical('diegoangel@hotmail.com'); 
+        $ownerUser->setEnabled(true); 
+        $ownerUser->setName('Owner User');
+        $ownerUser->setRoles(array('ROLE_OWNER'));
+        $encoder = $factory->getEncoder($ownerUser);
+        $password = $encoder->encodePassword($testPassword, $ownerUser->getSalt());
+        $ownerUser->setPassword($password);
+
+        $manager->updateUser($ownerUser);
+
+        $this->addReference('owner-user', $ownerUser);
 
         /** Redeemar User */
 
-        $manager->persist($systemUser);
-        $manager->flush();
+        $faker = \Faker\Factory::create();
+        $faker->seed(1234);
+
+        for ($i = 0; $i < 10; $i++)
+        {
+            $redeemarUser = $manager->createUser();
+
+            $fakeUsername = $faker->userName;
+            $redeemarUser->setUsername($fakeUsername);
+            $ownerUser->setUsernameCanonical($fakeUsername);
+            $fakeEmail = $faker->email;
+            $redeemarUser->setEmail($fakeEmail);
+            $redeemarUser->setEmailCanonical($fakeEmail);            
+            $redeemarUser->setEnabled(true);
+            $redeemarUser->setName($faker->Name);
+            $redeemarUser->setRoles(array('ROLE_REDEEMAR_USER'));
+            $encoder = $factory->getEncoder($redeemarUser);
+            $password = $encoder->encodePassword($testPassword, $redeemarUser->getSalt());
+            $redeemarUser->setPassword($password);
+
+            $manager->updateUser($redeemarUser);
+
+        }
+
+        $populator = new \Faker\ORM\Doctrine\Populator($faker, $objectManager);
+        $populator->addEntity('AppBundle:Category', 3);
+        $populator->addEntity('AppBundle:Logo', 5);
+        $populator->addEntity('AppBundle:Company', 5);
+        $populator->addEntity('AppBundle:Location', 5);        
+
+        $populator->execute();  
+
     }
+
+    /**
+     * Get the order of this fixture
+     *
+     * @return integer
+     */
+    function getOrder()
+    {
+        return 1;
+    }
+
 }
