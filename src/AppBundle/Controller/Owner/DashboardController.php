58933ad1d0e096db2b23c45b4c6fa1d6a4f75055
Diego <diegoangel77@rocketmail.com>
2016-02-15 13:46:16 -0300

    Development Owner section

---
+++ src/AppBundle/Controller/Owner/DashboardController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Controller\Owner;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Offer;
+
+/**
+ * @Route("/owner", name="owner")
+ */
+class DashboardController extends Controller
+{
+    /**
+     * @Route("/dashboard", name="owner_dashboard")
+     */
+    public function indexAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $offers = $em->getRepository('AppBundle:Offer')->findAll();
+
+        return $this->render('AppBundle:owner:dashboard:index.html.twig', array(
+            'offers' => $offers,
+        ));
+    }
+}
