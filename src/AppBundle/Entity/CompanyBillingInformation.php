9d9d28088127f7d3969034f4f95503080eaf3211
Diego <diegoangel77@rocketmail.com>
2016-02-18 10:04:42 -0300

    Add entity

--- src/AppBundle/Entity/CompanyBillingInformation.php
+++ src/AppBundle/Entity/CompanyBillingInformation.php
@@ -5,12 +5,12 @@ namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * Company
+ * CompanyBillingInformation
  *
- * @ORM\Table(name="company", uniqueConstraints={@ORM\UniqueConstraint(name="uidx_name", columns={"name"})}, indexes={@ORM\Index(name="fk_user_id", columns={"user_id"}), @ORM\Index(name="fk_category_id", columns={"category_id"}), @ORM\Index(name="fk_logo_id", columns={"logo_id"})})
+ * @ORM\Table(name="company_billing_information", indexes={@ORM\Index(name="fk_company_id", columns={"company_id"})})
  * @ORM\Entity
  */
-class Company
+class CompanyBillingInformation
 {
     /**
      * @var integer
@@ -24,261 +24,62 @@ class Company
     /**
      * @var string
      *
-     * @ORM\Column(name="name", type="string", length=255, nullable=false)
-     */
-    private $name;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="website", type="string", length=100, nullable=false)
-     */
-    private $website;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="description", type="text", length=65535, nullable=false)
-     */
-    private $description;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="address", type="string", length=255, nullable=false)
+     * @ORM\Column(name="address", type="string", length=255, nullable=true)
      */
     private $address;
 
     /**
+     * @var integer
+     *
+     * @ORM\Column(name="number", type="integer", length=6, nullable=true)
+     */
+    private $number;
+
+    /**
      * @var string
      *
-     * @ORM\Column(name="video", type="text", length=65535, nullable=false)
+     * @ORM\Column(name="apartment", type="string", length=10, nullable=true)
      */
-    private $video;
+    private $apartment;
 
     /**
-     * @var \AppBundle\Entity\User
+     * @var string
      *
-     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\Column(name="city", type="string", length=255, nullable=true)
+     */
+    private $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="state", type="string", length=255, nullable=true)
+     */
+    private $state;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="zip", type="string", length=10, nullable=true)
+     */
+    private $zip;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="country", )
+     */
+    private $country;
+
+    /**
+     * @var \AppBundle\Entity\Company
+     *
+     * @ORM\OneToOne(targetEntity="AppBundle\Entity\Company")
      * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     *   @ORM\JoinColumn(name="company_id", referencedColumnName="id")
      * })
      */
-    private $user;
-
-    /**
-     * @var \AppBundle\Entity\Category
-     *
-     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Category")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="category_id", referencedColumnName="id")
-     * })
-     */
-    private $category;
-
-    /**
-     * @var \AppBundle\Entity\Logo
-     *
-     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Logo")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="logo_id", referencedColumnName="id")
-     * })
-     */
-    private $logo;
+    private $company;
 
 
 
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set name
-     *
-     * @param string $name
-     * @return Company
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * Get name
-     *
-     * @return string 
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * Set website
-     *
-     * @param string $website
-     * @return Company
-     */
-    public function setWebsite($website)
-    {
-        $this->website = $website;
-
-        return $this;
-    }
-
-    /**
-     * Get website
-     *
-     * @return string 
-     */
-    public function getWebsite()
-    {
-        return $this->website;
-    }
-
-    /**
-     * Set description
-     *
-     * @param string $description
-     * @return Company
-     */
-    public function setDescription($description)
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    /**
-     * Get description
-     *
-     * @return string 
-     */
-    public function getDescription()
-    {
-        return $this->description;
-    }
-
-    /**
-     * Set address
-     *
-     * @param string $address
-     * @return Company
-     */
-    public function setAddress($address)
-    {
-        $this->address = $address;
-
-        return $this;
-    }
-
-    /**
-     * Get address
-     *
-     * @return string 
-     */
-    public function getAddress()
-    {
-        return $this->address;
-    }
-
-    /**
-     * Set video
-     *
-     * @param string $video
-     * @return Company
-     */
-    public function setVideo($video)
-    {
-        $this->video = $video;
-
-        return $this;
-    }
-
-    /**
-     * Get video
-     *
-     * @return string 
-     */
-    public function getVideo()
-    {
-        return $this->video;
-    }
-
-    /**
-     * Set user
-     *
-     * @param \AppBundle\Entity\User $user
-     * @return Company
-     */
-    public function setUser(\AppBundle\Entity\User $user = null)
-    {
-        $this->user = $user;
-
-        return $this;
-    }
-
-    /**
-     * Get user
-     *
-     * @return \AppBundle\Entity\User 
-     */
-    public function getUser()
-    {
-        return $this->user;
-    }
-
-    /**
-     * Set category
-     *
-     * @param \AppBundle\Entity\Category $category
-     * @return Company
-     */
-    public function setCategory(\AppBundle\Entity\Category $category = null)
-    {
-        $this->category = $category;
-
-        return $this;
-    }
-
-    /**
-     * Get category
-     *
-     * @return \AppBundle\Entity\Category 
-     */
-    public function getCategory()
-    {
-        return $this->category;
-    }
-
-    /**
-     * Set logo
-     *
-     * @param \AppBundle\Entity\Logo $logo
-     * @return Company
-     */
-    public function setLogo(\AppBundle\Entity\Logo $logo = null)
-    {
-        $this->logo = $logo;
-
-        return $this;
-    }
-
-    /**
-     * Get logo
-     *
-     * @return \AppBundle\Entity\Logo 
-     */
-    public function getLogo()
-    {
-        return $this->logo;
-    }
 }
